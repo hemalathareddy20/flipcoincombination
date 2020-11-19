3152b52640dc205a330c6b15fd4d063fe209f87d
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-19 16:37:36 +0530

    Added: logic to flipcoin multiple times and store singlet combination in dictionary and finally display percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,19 @@
+#!/bin/bash 
 echo "Welcome of the flip coin combination program being sloved in the Master branch"
+declare -A singlet
+read -p "enter a range to flip coin multiple times: " n
+range=$n
+while(( $n !=0))
+do
 random=$((RANDOM%2))
 echo "$random"
 if(($random==1))
 then
-     echo "Heads"
+      ((hcount++))
 else
-    echo "Tails"
+       ((tcount++))
 fi
+ ((n--))
+done
+echo "Heads(h) percentage" `echo $hcount $range | awk '{print ($1/$2)*100}'` "%"
+echo "Tails(t) percentage" `echo $tcount $range | awk '{print ($1/$2)*100}'` "%"
