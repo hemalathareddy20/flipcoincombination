12c87c589cd585399220be9457a89f38b6911556
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-19 15:54:37 +0530

    Added: random logic to display head or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,9 @@
 echo "Welcome of the flip coin combination program being sloved in the Master branch"
+random=$((RANDOM%2))
+echo "$random"
+if(($random==1))
+then
+     echo "Heads"
+else
+    echo "Tails"
+fi
