f7931d24d7ad347db8c5de2069716d3f74ab9835
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-19 18:44:06 +0530

    Added:logic to doublet combination and display percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -5,15 +5,33 @@ read -p "enter a range to flip coin multiple times: " n
 range=$n
 while(( $n !=0))
 do
+double=$((RANDOM%2))
 random=$((RANDOM%2))
-echo "$random"
+echo -n " doublet=$double$random "
+echo -n " singlet=$random "
 if(($random==1))
 then
       ((hcount++))
 else
        ((tcount++))
 fi
+if(($double==1 && $random==1))
+then
+      ((hhcount++))
+elif(($double==1 && $random==0))
+then
+       ((htcount++))
+elif(($double==0 && $random==0))
+then
+       ((ttcount++))
+else
+      ((thcount++))
+fi
  ((n--))
 done
 echo "Heads(h) percentage" `echo $hcount $range | awk '{print ($1/$2)*100}'` "%"
 echo "Tails(t) percentage" `echo $tcount $range | awk '{print ($1/$2)*100}'` "%"
+echo "HH percentage" `echo $hhcount $range | awk '{print ($1/$2)*100}'` "%"
+echo "TT percentage" `echo $ttcount $range | awk '{print ($1/$2)*100}'` "%"
+echo "HT percentage" `echo $htcount $range | awk '{print ($1/$2)*100}'` "%"
+echo "TH percentage" `echo $thcount $range | awk '{print ($1/$2)*100}'` "%"
